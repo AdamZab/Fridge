944a2b92cab779f069724f8d79ca9b25090f5d84
AdamZab <cantivore@gmail.com>
2019-08-25 23:17:08 +0200

    scale name

--- fridge.cpp/calibration.cpp.ino
+++ fridge.cpp/calibration.cpp.ino
@@ -1,27 +1,37 @@
-#include "HX711.h" 
- 
-const int LOADCELL1_DOUT_PIN = 6;
-const int LOADCELL1_SCK_PIN = 5;
-HX711 scale1;
-const int LOADCELL2_DOUT_PIN = 11;
-const int LOADCELL2_SCK_PIN = 10;
-HX711 scale2;
+/*#include "HX711.h" 
+
+const int NUMBER_OF_SCALES = 2;
+const char LOADCELL_SDA_PIN[NUMBER_OF_SCALES] = {6, 11};
+const char LOADCELL_SCL_PIN[NUMBER_OF_SCALES] = {5, 10};
+HX711 scale[NUMBER_OF_SCALES];
+#define NUMBER_OF_SCALE_READINGS = 100
+#define KNOWN_WEIGHT_IN_KG = 0.145;
+
  
 void setup() {
-  Serial.begin(9600);
-  scale1.begin(LOADCELL1_DOUT_PIN, LOADCELL1_SCK_PIN);
-  scale1.setScale();  
-  scale1.tare(); 
-  scale2.begin(LOADCELL2_DOUT_PIN, LOADCELL2_SCK_PIN);
-  scale2.setScale();  
-  scale2.tare();
+    Serial.begin(9600);
+    for(int scaleID = 0; scaleID < NUMBER_OF_SCALES; ++scaleID){
+        scale[scaleID].begin(LOADCELL_SDA_PIN[scaleID], LOADCELL_SCL_PIN[scaleID]);  
+        scale[scaleID].tare();    
+    } 
 }
 
-void loop() {
+calibration() {
+    for(int scaleID = 0; scaleID < NUMBER_OF_SCALES; ++scaleID){
+        float currentWeight = scale[scaleID].get_units(NUMBER_OF_SCALE_READINGS);
+        float scaleFactor=(currentWeight/KNOWN_WEIGHT_IN_KG = 0.145;);
+        String serialPortMessage = "Scale factor for scale ";
+        serialPortMessage += String(scaleID);
+        serialPortMessage += ": ";
+        serialPortMessage += String(scaleFactor);
+        Serial.println(serialPortMessage);
+    }
+    return currentWeight;
+}  
+  float scaleFactor0=(currentWeight0/0.145);  // znana waga
+  Serial.println(scaleFactor0);              // wartosc uzyta w scale.setScale();
   float currentWeight1=scale.get1_units(20);  
   float scaleFactor=(currentWeight1/0.145);  // znana waga
-  Serial.println(scaleFactor1);              // wartosc uzyta w scale1.setScale();
-  float currentWeight1=scale.get1_units(20);  
-  float scaleFactor=(currentWeight1/0.145);  // znana waga
-  Serial.println(scaleFactor1);              // wartosc uzyta w scale2.setScale();
+  Serial.println(scaleFactor1);              // wartosc uzyta w scale.setScale();
 }
+*/
